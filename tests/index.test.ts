c8f22199252f42d0f9fd2ed7aee1daf2345e2bf1
Carlos Muñoz <munoz.josecarlos@gmail.com>
2025-04-26 03:37:11 +0000

    added yarn test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,7 +6,9 @@ describe('testing binary search sort', () => {
   });
 
   test('ordered array', () => {
-    expect(binaryInsertionSort([9, 5, 4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4, 5, 9]);
+    let unordered: number[] = [9, 5, 4, 3, 2, 1];
+    let ordered: number[] = [1, 2, 3, 4, 5, 9];
+    expect(binaryInsertionSort(unordered)).toStrictEqual(ordered);
 
   })
 
